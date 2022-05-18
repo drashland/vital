e14b0fce9f4678e3bb551068ff754ef997dba4dc
drashbot <59404681+drashbot@users.noreply.github.com>
2022-05-18 00:32:28 +0000

    chore: update deps

--- tests/deps.ts
+++ tests/deps.ts
@@ -3,7 +3,7 @@ export {
   AbstractSeed,
   ClientPostgreSQL,
 } from "https://deno.land/x/nessie@2.0.6/mod.ts";
-export { assertEquals } from "https://deno.land/std@0.138.0/testing/asserts.ts";
+export { assertEquals } from "https://deno.land/std@0.139.0/testing/asserts.ts";
 export type { Info } from "https://deno.land/x/nessie@2.0.6/mod.ts";
 export {
   migrate,
