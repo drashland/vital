d46a6845136da926300a4690654747d30c196276
drashbot <59404681+drashbot@users.noreply.github.com>
2024-03-16 00:30:52 +0000

    chore: update deps

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export {
   Client as PostgresClient,
   Pool,
-} from "https://deno.land/x/postgres@v0.16.0/mod.ts";
+} from "https://deno.land/x/postgres@v0.19.3/mod.ts";
 export type {
   ClientOptions,
-} from "https://deno.land/x/postgres@v0.16.0/mod.ts";
-export { config } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
+} from "https://deno.land/x/postgres@v0.19.3/mod.ts";
+export { config } from "https://deno.land/x/dotenv@v3.2.2/mod.ts";
